07b33331f7865f0302f3320b147833473b6df790
Umer Waseem <60791855+umerwaseem4@users.noreply.github.com>
2021-03-15 15:48:29 +0500

    added delete fn

---
+++ assets/js/index.js
@@ -0,0 +1,18 @@
+if (window.location.pathname === "/") {
+  $ondelete = $(".table tbody td a.delete");
+  $ondelete.click(function () {
+    const id = $(this).attr("data-id");
+
+    var request = {
+      url: `http://localhost:5000/api/users/${id}`,
+      method: "DELETE",
+      date: data,
+    };
+    if (confirm("Do you really want to delete this record?")) {
+      $.ajax(request).done(function (response) {
+        alert("Data Deleted Successfully!");
+        location.reload();
+      });
+    }
+  });
+}
